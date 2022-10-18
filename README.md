cbd70448a4bc92d2178d43e122481da47bac6ace
Andrew Horvath <skwerl@student.fullsail.edu>
2022-10-17 20:28:31 -0500

    getting started

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SSL_Wk3_Assignment4
+
+## Andrew Horvath
+
+### Async and synchronous assignment
+
+Correcting an app that has incorrect and or missing code.
---
+++ index.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const myName = ( () => {
+  console.log("Here is my IP address");
+});
+
+callHttpBin( () => {
+  let promise = new Promise((resolve, reject) => {
+    http.get(
+     'http://httpbin.org/ip',
+     function(response) {
+      const str="";
+      response.setEncoding('utf8');
+      response.on('data', (data) => {
+      str += data;
+     });
+     response.on('end', function() {
+      var result = JSON.parse(str);
+      myips = result.origin;
+      resolve()
+     });
+     }
+    );
+});
+
+let result = await promise;
+result;
+});
+
+executeAsyncTask( () => {
+  const valueA = await callHttpBin()
+  const valueB = myName();
+  console.log(valueB+" "+valueA)
+});
+
+// Output Here is my IP address 149.24.160.1
