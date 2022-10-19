9f7cd2f33b316c2e221c506cb80edd67c2f8b6c0
Andrew Horvath <skwerl@student.fullsail.edu>
2022-10-19 00:20:33 -0500

    fixed and removed files. refactored code

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-// const http = require('http');
-const myName = function(){
-  console.log("Here is my IP address");
+const http = require('http'); // change var to const and added p to http 
+const myName = function(){ //change var to const, camel case for variable name, added letter i to misspelled function
+    return "Here is my IP address"; //changed console log to return a sentence as it doesn't make sense to try and console log a console log.
+}; //added closing semi-colon
+
+function callHttpBin() { //camel case for entire function name and added n to fix/finish name properly, added async
+    return new Promise((resolve, reject) => {
+        http.get(
+            'http://httpbin.org/ip',
+                function(response) {
+                    let str=""; //changed var to let to allow value changes
+                    response.setEncoding('utf8');
+                        response.on('data', function(data){
+                            str += data;
+                        });
+                    response.on('end', function() {
+                    const result = JSON.parse(str); //changed var to const
+                    const myIps = result.origin; //added variable declaration 
+                    resolve(myIps); //added the ip address variable in the response and closing semi-colon 
+                   
+                    });
+                } 
+        );
+    
+    });
+    
+//   let result = promise; removed this as the resolve handles the passing of the data
+//     return result;
 };
 
-async function callHttpBin() {
-  let promise = new Promise((resolve, reject) => {
-    console.log("Here");
-    fetch(
-     'http://httpbin.org/ip',
-     (response) => {
-      const str="";
-      response.setEncoding('utf8');
-      response.on('data', (data) => {
-      str += data;
-      console.log(data);
-     });
+async function executeAsyncTask(){ //added async
+    const valueA = await callHttpBin(); //fixed function name to match declaration
+    const valueB = myName(); //fixed variable name to match declaration 
+  console.log(valueB + " " + valueA); //added closing semi-colon
+} //added closing bracket
 
-     response.on('end', function() {
-      let result = JSON.parse(str);
-      let myIps = result.origin;
-      resolve();
-     });
-     }
-    );
-});
-   let result = await promise;
-   return result;
-};
-
-async function executeAsyncTask(){
-  const valueA = await callHttpBin();
-  const valueB = myName();
-//   console.log(valueB+" "+valueA);
-console.log(valueB);
-};
-// Output Here is my IP address 149.24.160.1
-
+// Output Here is IP address 149.24.160.1
 executeAsyncTask();
